9ceb194474a25bf8ba6c7fc34bee1e4ee69bd652
Cecilia <tony.cecilia1822@gmail.com>
2021-01-18 15:25:19 +0100

    Add update td4

--- app/src/main/java/com/example/newslist/LoginActivity.java
+++ app/src/main/java/com/example/newslist/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.newslist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,18 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setTitle(getLocalClassName());
+
+        //On déclare une variable bouton qui prend les caractéristiques du button avec l'id "button"
+        Button rollButton = (Button) findViewById(R.id.buttonLogin);
+
+        // On donne à ce boutton une action quand on clique dessus
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(this, NewsActivity.class); startActivity(intent);
+
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/newslist/NewsActivity.java
+++ app/src/main/java/com/example/newslist/NewsActivity.java
@@ -1,6 +1,12 @@
 package com.example.newslist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +17,18 @@ public class NewsActivity  extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
         setTitle(getLocalClassName());
+
+        //On déclare une variable bouton qui prend les caractéristiques du button avec l'id "button"
+        Button rollButton = (Button) findViewById(R.id.button);
+
+        // On donne à ce boutton une action quand on clique dessus
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(this, AutreActivity.class); startActivity(intent);
+
+
+            }
+        });
     }
 }
